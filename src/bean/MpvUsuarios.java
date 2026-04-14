0f8d0f810a7f7213cfc7ea4b8cb47793b3cd524d
ivanzitus <bielivan219@gmail.com>
2026-04-14 09:14:51 -0400

    coisas doidas

---
+++ src/bean/MpvUsuarios.java
@@ -0,0 +1,135 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bean;
+
+import java.util.Date;
+
+/**
+ *Imagina ele usando o efeiro do lobisome 
+ * @author u05661916108
+ */
+public class MpvUsuarios {
+    private int mpvIdUsuarios;
+    private String mpvNome;
+    private String mpvApelido;
+    private String mpvCpf;
+    private Date mpvDataNacimento;
+    private int mpvNivel;
+    private String mpvSenha;
+    private String mpvArivo;
+
+    /**
+     * @return the mpvIdUsuarios
+     */
+    public int getMpvIdUsuarios() {
+        return mpvIdUsuarios;
+    }
+
+    /**
+     * @param mpvIdUsuarios the mpvIdUsuarios to set
+     */
+    public void setMpvIdUsuarios(int mpvIdUsuarios) {
+        this.mpvIdUsuarios = mpvIdUsuarios;
+    }
+
+    /**
+     * @return the mpvNome
+     */
+    public String getMpvNome() {
+        return mpvNome;
+    }
+
+    /**
+     * @param mpvNome the mpvNome to set
+     */
+    public void setMpvNome(String mpvNome) {
+        this.mpvNome = mpvNome;
+    }
+
+    /**
+     * @return the mpvApelido
+     */
+    public String getMpvApelido() {
+        return mpvApelido;
+    }
+
+    /**
+     * @param mpvApelido the mpvApelido to set
+     */
+    public void setMpvApelido(String mpvApelido) {
+        this.mpvApelido = mpvApelido;
+    }
+
+    /**
+     * @return the mpvCpf
+     */
+    public String getMpvCpf() {
+        return mpvCpf;
+    }
+
+    /**
+     * @param mpvCpf the mpvCpf to set
+     */
+    public void setMpvCpf(String mpvCpf) {
+        this.mpvCpf = mpvCpf;
+    }
+
+    /**
+     * @return the mpvDataNacimento
+     */
+    public Date getMpvDataNacimento() {
+        return mpvDataNacimento;
+    }
+
+    /**
+     * @param mpvDataNacimento the mpvDataNacimento to set
+     */
+    public void setMpvDataNacimento(Date mpvDataNacimento) {
+        this.mpvDataNacimento = mpvDataNacimento;
+    }
+
+    /**
+     * @return the mpvNivel
+     */
+    public int getMpvNivel() {
+        return mpvNivel;
+    }
+
+    /**
+     * @param mpvNivel the mpvNivel to set
+     */
+    public void setMpvNivel(int mpvNivel) {
+        this.mpvNivel = mpvNivel;
+    }
+
+    /**
+     * @return the mpvSenha
+     */
+    public String getMpvSenha() {
+        return mpvSenha;
+    }
+
+    /**
+     * @param mpvSenha the mpvSenha to set
+     */
+    public void setMpvSenha(String mpvSenha) {
+        this.mpvSenha = mpvSenha;
+    }
+
+    /**
+     * @return the mpvArivo
+     */
+    public String getMpvArivo() {
+        return mpvArivo;
+    }
+
+    /**
+     * @param mpvArivo the mpvArivo to set
+     */
+    public void setMpvArivo(String mpvArivo) {
+        this.mpvArivo = mpvArivo;
+    }
+    
+}
